b3586f08d03ee8c1db2fe76af8e01917a55f5b27
Hilker Bazan Flores <loandevpy@gmail.com>
2025-01-31 22:04:21 -0500

    implementamos el contador de intentos

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = generarNumeroSecreto();
+let intentos = 1;
 
 function asignarTextoElemento(elemento, texto ) {
     let elementoHTML = document.querySelector(elemento);
@@ -11,13 +12,14 @@ function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
 
     if (numeroDeUsuario === numeroSecreto) {
-        asignarTextoElemento('p','Acertaste el número');
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos == 1 ) ? 'vez' : 'veces'}`);
     } else {
         if (numeroDeUsuario > numeroSecreto) {
             asignarTextoElemento('p', 'El número secreto es menor');
         } else {
             asignarTextoElemento('p', 'El número secreto es mayor');
         }
+        intentos++;
     }
     return;
 }
