2f214c32a620516eb4e7a90575361ccc73518e7d
Hilker Bazan Flores <loandevpy@gmail.com>
2025-01-31 22:54:39 -0500

    desbloqueamos el boton de nuevo juego tras ganar

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let numeroSecreto = generarNumeroSecreto();
-let intentos = 1;
+let numeroSecreto = 0;
+let intentos = 0;
 
 function asignarTextoElemento(elemento, texto ) {
     let elementoHTML = document.querySelector(elemento);
@@ -35,5 +35,22 @@ function generarNumeroSecreto() {
     return Math.floor(Math.random()*10)+1;
 }
 
-asignarTextoElemento('h1','Juego del número secreto!');
-asignarTextoElemento('p','Indica un número del 1 al 100');
+function condicionesIniciales() {
+    asignarTextoElemento('h1','Juego del número secreto!');
+    asignarTextoElemento('p','Indica un número del 1 al 100');
+    // Generarl el numero aleatorio
+    numeroSecreto = generarNumeroSecreto();
+    // Inicializar el número de intentos
+    intentos = 1;
+}
+
+function reiniciarJuego() {
+    // Limpiar la caja
+    limpiarCaja();
+    // Indicar mensaje de intervalo de números
+    condicionesIniciales();
+    // Deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true')
+}
+
+mensajesIniciales();
