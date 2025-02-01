08afd51e342fb057408885df1880dbf5c685f315
Hilker Bazan Flores <loandevpy@gmail.com>
2025-01-31 21:32:40 -0500

    agregamos el numero random

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+let numeroSecreto = generarNumeroSecreto();
+
+console.log(numeroSecreto);
+
 function asignarTextoElemento(elemento, texto ) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 
 }
 function intentoDeUsuario(){
     alert('click desde el botón')
+    return;
+}
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
 }
 
 asignarTextoElemento('h1','Juego del número secreto!');
